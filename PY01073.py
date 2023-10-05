69bf97c33deba84a2d55eb5f6ed54e8f324532d4
Hosanguyen <nt.hoa141203@gmail.com>
2023-10-05 18:35:51 +0700

    update

--- PY01073.py
+++ PY01073.py
@@ -1,23 +1,8 @@
-s = input()
-x = []
-check = []
-n = len(s)
-for i in range(n+1):
-    x += [0]
-    check += [False]
-def display():
-    for i in range(1, n+1):
-        print(s[x[i]-1], end = '')
-    print()
-def hoanvi(i):
-    for j in range(1, n+1):
-        if(check[j]==False):
-            x[i] = j
-            check[j] = True
-            if(i==n):
-                display()
-            else:
-                hoanvi(i+1)
-            check[j] = False
+from itertools import permutations
 
-hoanvi(1)
+s = input()
+
+words = [''.join(p) for p in permutations(s)]
+
+for x in words:
+    print(x)
